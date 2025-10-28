304e50e264c4249ffd4b66bbf2320c845d52c177
abishekdasarathan <abishekdasarathan@gmail.com>
2025-10-28 16:41:52 +0530

    Add files via upload

---
+++ TASK 2.py
@@ -0,0 +1,26 @@
+name="ABISHEK"
+age= 26
+height= 177.5
+weight=87
+city= "ARUPPUKOTTAI"
+is_student= False
+marks= 7.05
+pi=3.14
+school_name="SBK Boys Hr Sec School"
+my_number=10
+my_text="Hello"
+print(name)
+print(age)
+print("Height in cm",height)
+print("Weight in kg",weight)
+print(city)
+print(is_student)
+print(marks)
+print(pi)
+print(school_name)
+print(type(name))
+print(type(age))
+print (type(pi))
+print (type(is_student))
+print(my_number,type(my_number))
+print(my_text,type(my_text))
